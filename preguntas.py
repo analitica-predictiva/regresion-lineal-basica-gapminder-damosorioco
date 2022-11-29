ae9fc455919230abfc71c9c84639a43f62e1fb9a
damosorioco <101427484+damosorioco@users.noreply.github.com>
2022-11-28 22:40:10 -0500

    P2

--- preguntas.py
+++ preguntas.py
@@ -48,7 +48,7 @@ def pregunta_02():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = pd.read_csv('gm_2008_region-csv')
+    df = pd.read_csv('gm_2008_region.csv')
 
     # Imprima las dimensiones del DataFrame
     print(df.shape)
